bdc30dabbf87270178e88ddf6635c507a148b932
nickborbe <nickborbe@gmail.com>
2019-06-03 07:45:25 -0400

    first test buildr

---
+++ public/precache-manifest.becee31314fddc3644b35dab715e72b6.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7b95dbdd87e0c809570affaf366a434",
+    "url": "/caso-client/static/media/fa-brands-400.a7b95dbd.eot"
+  },
+  {
+    "revision": "09dbeb662077bf491196",
+    "url": "/caso-client/static/css/main.ee027078.chunk.css"
+  },
+  {
+    "revision": "6d74c613f956b0c0e6ad",
+    "url": "/caso-client/static/js/runtime~main.e0f4ba76.js"
+  },
+  {
+    "revision": "9a34c63af7fd3ef01d24",
+    "url": "/caso-client/static/js/2.3426cfb2.chunk.js"
+  },
+  {
+    "revision": "e6257a726a0cf6ec8c6fec22821c055f",
+    "url": "/caso-client/static/media/fa-regular-400.e6257a72.woff2"
+  },
+  {
+    "revision": "427d721b86fc9c68b2e85ad42b69238c",
+    "url": "/caso-client/static/media/fa-regular-400.427d721b.woff"
+  },
+  {
+    "revision": "dcce4b7fbd5e895561e18af4668265af",
+    "url": "/caso-client/static/media/fa-regular-400.dcce4b7f.eot"
+  },
+  {
+    "revision": "65b9977aa23185e8964b36eddbce7a20",
+    "url": "/caso-client/static/media/fa-regular-400.65b9977a.ttf"
+  },
+  {
+    "revision": "5e2f92123d241cabecf0b289b9b08d4a",
+    "url": "/caso-client/static/media/fa-brands-400.5e2f9212.woff2"
+  },
+  {
+    "revision": "418dad87601f9c8abd0e5798c0dc1feb",
+    "url": "/caso-client/static/media/fa-solid-900.418dad87.woff2"
+  },
+  {
+    "revision": "2ef8ba3410dcc71578a880e7064acd7a",
+    "url": "/caso-client/static/media/fa-brands-400.2ef8ba34.woff"
+  },
+  {
+    "revision": "a7140145ebaaf5fb14e40430af5d25c4",
+    "url": "/caso-client/static/media/fa-solid-900.a7140145.woff"
+  },
+  {
+    "revision": "98b6db59be947f563350d2284fc9ea36",
+    "url": "/caso-client/static/media/fa-brands-400.98b6db59.ttf"
+  },
+  {
+    "revision": "09dbeb662077bf491196",
+    "url": "/caso-client/static/js/main.d052b76d.chunk.js"
+  },
+  {
+    "revision": "5eb754ab7dbd2fee562360528db4c3c0",
+    "url": "/caso-client/static/media/fa-regular-400.5eb754ab.svg"
+  },
+  {
+    "revision": "ff8d9f8adb0d09f11d4816a152672f53",
+    "url": "/caso-client/static/media/fa-solid-900.ff8d9f8a.ttf"
+  },
+  {
+    "revision": "46e7cec623d8bd790d9fdbc8de2d3ee7",
+    "url": "/caso-client/static/media/fa-solid-900.46e7cec6.eot"
+  },
+  {
+    "revision": "5bf145531213545e03ff41cd27df7d2b",
+    "url": "/caso-client/static/media/fa-brands-400.5bf14553.svg"
+  },
+  {
+    "revision": "49279363201ed19a840796e05a74a91b",
+    "url": "/caso-client/static/media/fa-solid-900.49279363.svg"
+  },
+  {
+    "revision": "81601ddd39951f2d7088333a597d7c5b",
+    "url": "/caso-client/static/media/ModenaSans-Regular.81601ddd.otf"
+  },
+  {
+    "revision": "3db6e9276721cadfed7b947425de42ac",
+    "url": "/caso-client/static/media/ModenaSans-Regular.3db6e927.ttf"
+  },
+  {
+    "revision": "752c13e1b881564f5f54afa1a4859492",
+    "url": "/caso-client/static/media/NoirPro-Regular.752c13e1.eot"
+  },
+  {
+    "revision": "d969db6adb881f1dfa91a5b7ec0154d9",
+    "url": "/caso-client/static/media/NoirPro-Regular.d969db6a.otf"
+  },
+  {
+    "revision": "789e17399e4db2953aed19fa2587f4e5",
+    "url": "/caso-client/static/media/NoirPro-Regular.789e1739.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/caso-client/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5f95e4e7372112d171c26b4ee83b762b",
+    "url": "/caso-client/index.html"
+  }
+];
